8112518a35338d315dfce16c734a025fc5e8251b
Ferdinand <ferdifenrir@gmail.com>
2017-10-12 19:04:01 +0700

    add interface

--- ewallet.py
+++ ewallet.py
@@ -8,7 +8,7 @@ import requests
 import json
 import yaml
 
-ip_address = '0.0.0.0'
+ip_address = '127.0.0.1'
 port = 5000
 file_path = os.path.abspath(os.getcwd())+"/ewallet.db"
 list_api = 'http://152.118.31.2/list.php'
@@ -44,7 +44,6 @@ def register():
       data = json.loads(request.data.decode())
       user_id = data['user_id']
       name = data['nama']
-      register_return = data['registerReturn']
       new_user = User(user_id=user_id, name=name, saldo=0)
       try:
         db.session.add(new_user)
@@ -90,19 +89,20 @@ def getSaldo():
     response['nilai_saldo'] = -99
     return Response(json.dumps(response, ensure_ascii=True)+"\n", status=200, mimetype=prod_json)
 
-@app.route(base_path + 'isQuorum', methods=['GET'])
 def isQuorum():
   # r = requests.get(list_api).text
   # r_json = json.loads(r)
   # ================================
   # count = 0
   # with open('test.json') as data_file:    
-  #   r_json = json.load(data_file)  
-  # for entry in r_json:
-  #   url = entry['ip'] + ':' + str(port) + '/ewallet/ping'
+  #   list_json = json.load(data_file)  
+  # for entry in list_json:
   #   try:
-  #     # r = requests.post(url).text
-  #     print (url)
+  #     url = 'http://'+entry['ip']+':' + str(port) + '/ewallet/ping'
+  #     r = requests.post(url).text
+  #     r_json = json.loads(r)
+  #     print (r_json['pong'])
+  #     # print (url)
   #   except Exception as ex:
   #     print (ex)
   return True
---
+++ interface.py
@@ -0,0 +1,63 @@
+import requests
+import json
+from models import User
+
+port = 5000
+
+def ping():
+  ip_address = input("Input IP to ping on: ")
+  url = 'http://'+ip_address+':' + str(port) + '/ewallet/ping'
+  r = requests.post(url).text
+  r_json = json.loads(r)
+  print ('return: ' + r_json['pong'])
+
+def get_saldo():
+  ip_address = input("Input IP to get saldo from: ")
+  all_user = User.query.all()
+  print ('Select your user id from your database')
+  for index, user in enumerate(all_user):
+    index += 1
+    print (str(index) + '. ' + str(user.name) + ' : ' + str(user.user_id))
+  user_id = input('Please input the user id: ')
+  url = 'http://'+ip_address+':' + str(port) + '/ewallet/getSaldo'
+  payload = {"user_id": str(user_id)}
+  r = requests.post(url, data=json.dumps(payload)).text
+  r_json = json.loads(r)
+  nilai_saldo = r_json['nilai_saldo']
+  if (nilai_saldo == -1):
+    try:
+      url = 'http://'+ip_address+':' + str(port) + '/ewallet/register'
+      user_name = User.query.filter_by(user_id=user_id).first().name
+      payload['user_id'] = {"user_id": str(user_id), "nama": str(user_name)}
+      r = requests.post(url, data=json.dumps(payload)).text
+      r_json = json.loads(r)
+      status = r_json['status_register']
+      if (status == -2):
+        print ('Quorum not enough')
+      elif (status == -4):
+        print ('Sorry.. Database error')
+      elif (status == -99):
+        print ('Unknown problem Captain!!!')
+      else:
+        print ("Successfully registered to: " + ip_address)
+    except Exception as ex:
+      print (ex)
+  elif (nilai_saldo == -2):
+    print ('Quorum not enough')
+  elif (status == -4):
+    print ('Sorry.. Database error')
+  elif (status == -99):
+    print ('Unknown problem Captain!!!')
+  else:
+    print ('Nilai saldo di ' + str(ip_address) + ' : ' + str(r_json['nilai_saldo']))
+
+user_input = ""
+while user_input != "quit":
+  user_input = input("User input: ")
+  if user_input == "ping":
+    ping()
+  elif user_input == "getsaldo":
+    get_saldo()
+  else:
+    print ("Invalid input")
+    
